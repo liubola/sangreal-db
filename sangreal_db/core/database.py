4ceff47823ab28a145ba57470ba698b5e641f322
切了特空 <lby3523@gmail.com>
2018-11-19 09:49:48 +0800

    add doc

--- sangreal_db/core/database.py
+++ sangreal_db/core/database.py
@@ -69,15 +69,43 @@ or <{table_name}> is not the right table name, such as {reprlib.repr(self.tables
         return tables
 
     def query(self, *columns):
+        """[session.query]
+        
+        Returns:
+            [Query] -- [sqlalchemy Query cls]
+        """
+
         return self._session.query(*columns)
 
     def update(self, t_obj):
+        """[update table]
+        
+        Arguments:
+            t_obj {[objs of DeclarativeMeta]} -- [update the table]
+        """
+
         if isinstance(t_obj, Iterable):
             self._session.add_all(t_obj)
         else:
             self._session.add(t_obj)
 
     def insert(self, table, insert_obj, ignore=True):
+        """[insert bulk data]
+        
+        Arguments:
+            table {[DeclarativeMeta cls]} -- [reflection of table]
+            insert_obj {[pd.DataFrame or list of dicts]} -- [insert_obj]
+        
+        Keyword Arguments:
+            ignore {bool} -- [wether ignore exception or not] (default: {True})
+        
+        Raises:
+            ValueError -- [f"The {reprlib.repr(insert_obj)} must be list of dicts type!"]
+        
+        Returns:
+            [type] -- [description]
+        """
+
         if isinstance(insert_obj, pd.DataFrame):
             insert_obj = insert_obj.to_dict(orient='records')
         elif not isinstance(insert_obj, list):
