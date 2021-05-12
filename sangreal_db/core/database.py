47c0b89e8df976e45d125b442874ab5e350c06f7
liubola <lby3523@gmail.com>
2021-05-12 09:37:53 +0800

    update for pandas

--- sangreal_db/core/database.py
+++ sangreal_db/core/database.py
@@ -152,7 +152,7 @@ or <{table_name}> is not the right table name, such as {reprlib.repr(self.tables
         if isinstance(insert_obj, pd.DataFrame):
             if insert_obj.empty:
                 raise ValueError('The input DataFrame is empty, please check!')
-            insert_obj = insert_obj.to_dict(orient='records')
+            insert_obj = insert_obj.to_dict('records')
         elif not isinstance(insert_obj, list):
             raise ValueError(
                 f"The {reprlib.repr(insert_obj)} must be list of dicts type!")
